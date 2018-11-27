e31f6fb3ec69dfdb743e9b1f8a54ab75081158c5
EllenMengqiXu <40548240+EllenMengqiXu@users.noreply.github.com>
2018-11-27 12:48:06 -0500

    Update time.py

--- time.py
+++ time.py
@@ -2,9 +2,11 @@
 # coding:utf8
 
 import time
+# local time will print out the timestamp of the current time.
 localtime = time.time()
 print (localtime)
 
 import datetime
+# It will print out the date format of current time.
 lt=datetime.datetime.fromtimestamp(localtime).strftime('%Y-%m-%d %H:%M:%S')
 print(lt)
