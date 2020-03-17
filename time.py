dfb13bac76959925081f80a6f247fef40aa573f3
exu-madisonlogic <exu@madisonlogic.com>
2020-03-17 15:39:39 -0400

    added comment

--- time.py
+++ time.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # coding:utf8
 
+
 import time
-# local time will print out the timestamp of the current time.
+# local time will print out the timestamp of the current time. #timestamp
 localtime = time.time()
 print (localtime)
 
 import datetime
-# It will print out the date format of current time.
+# It will print out the date format of current time. #human friendly
+
 lt=datetime.datetime.fromtimestamp(localtime).strftime('%Y-%m-%d %H:%M:%S')
 print(lt)
